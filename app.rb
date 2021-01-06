85cf71d46fd853cea3af02b59f0863c1eec4a07b
Cintia <cintia_gloker@hotmail.com>
2021-01-06 13:47:44 +0000

    Adding erb files for html code

--- app.rb
+++ app.rb
@@ -8,9 +8,6 @@ get '/secret'do
  "it is a secret!!"
 end
 
-
 get '/cat' do
-"<div style='border: dashed red'>
-  <img src='https://image.shutterstock.com/image-photo/cat-medical-mask-protective-antiviral-260nw-1684423789.jpg'>
-</div>"
+  erb(:index)
 end
